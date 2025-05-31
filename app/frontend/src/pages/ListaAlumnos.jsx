56051087af8908c2de025a534cc9c28deb49e904
isaac13 <isaacjoelillatarco@gmail.com>
2025-05-30 20:12:54 -0400

    Component Navegador, Page ListaAlumnos y modificacion de uso del Component Boton

---
+++ app/frontend/src/pages/ListaAlumnos.jsx
@@ -0,0 +1,156 @@
+import '../styles/ListaAlumnos.css'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Admin from '../assets/admin.png'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Navegador from '../components/Navegador'
+
+import { useSearchParams } from 'react-router-dom';
+
+export const ListaAlumnos = () => {
+    const [searchParams] = useSearchParams();
+    const curso = searchParams.get('curso');
+    return (
+        <Container fluid className='ps-0 pe-0 contenido'>
+            <Navegador icono={Admin} user="user" dato={curso}/>
+            <Row md={4} className='m-3'>
+                <Col className='me-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='m-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='m-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='m-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='m-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='m-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='m-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='m-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='m-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='m-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className='m-0'>
+                    <Card className='carta'>
+                        <Card.Body >
+                            <Card.Title>Nombre estudiante</Card.Title>
+                                <ul>
+                                    <li><strong>datos:</strong>aaaaa</li>
+                                </ul>
+                            <Button className='m-1 boton-carta' variant="primary">Editar</Button>
+                            <Button className='m-1 boton-carta' variant="danger">Eliminar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+export default ListaAlumnos
--- app/frontend/src/pages/Perfil.jsx
+++ app/frontend/src/pages/Perfil.jsx
@@ -4,8 +4,6 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
 import { Boton } from '../components/Boton'
 
 import Admin from '../assets/admin.png'
@@ -16,7 +14,7 @@ export const Perfil = () => {
     return (
         <Container className='body' fluid>
             <Row>
-                <Col className="d-flex flex-column align-items-center text-center carta" md={4}>
+                <Col className="d-flex flex-column align-items-center text-center carta-perfil" md={4}>
                     <Image src={Admin} roundedCircle className='icono'/>
                     <div className='datos'>
                         <h5 className='nombre-user'>Nombre de usuario</h5>
@@ -26,21 +24,24 @@ export const Perfil = () => {
                 <Col md={{ span: 6, offset: 2 }}>
                     <h2 className='titulo-cursos'>Cursos Disponibles</h2>
                     <Row md={3} className='borde'>
-                        <div className='botones'>
-                            <Boton texto="Matematica"/>
-                            <Boton texto="hola"/>
-                            <Boton texto="hola"/>
-                        </div>
-                        <div className='botones'>
-                            <Boton texto="hola"/>
-                            <Boton texto="hola"/>
-                            <Boton texto="hola"/>
-                        </div>
-                        <div className='botones'>
-                            <Boton texto="hola"/>
-                            <Boton texto="hola"/>
-                            <Boton texto="hola"/>
-                        </div>
+                        <Col className='botones'>
+                            <Boton texto="hola" ruta='/alumnos?curso=curso'/>
+                        </Col>
+                        <Col className='botones'>
+                            <Boton texto="hola" ruta='/alumnos?curso=curso'/>
+                        </Col>
+                        <Col className='botones'>
+                            <Boton texto="hola" ruta='/alumnos?curso=curso'/>
+                        </Col>
+                        <Col className='botones'>
+                            <Boton texto="hola" ruta='/alumnos?curso=curso'/>
+                        </Col>
+                        <Col className='botones'>
+                            <Boton texto="hola" ruta='/alumnos?curso=curso'/>
+                        </Col>
+                        <Col className='botones'>
+                            <Boton texto="hola" ruta='/alumnos?curso=curso'/>
+                        </Col>
                     </Row>
                     <h2 className='titulo-datos'>Datos</h2>
                     <Row className='borde'>
